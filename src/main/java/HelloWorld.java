db7f8a75c5eadd9e60f31dbf613c38cf5ee91b75
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2019-02-05 09:37:38 +0200

    Gradle and TestNG Hello World

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public String foo() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+// https://stackoverflow.com/questions/155436/unit-test-naming-best-practices
+// http://osherove.com/blog/2005/4/3/naming-standards-for-unit-tests.html
+
+public class HelloWorldTest {
+
+	@Test()
+	public void Foo_ReturnsHelloWorld() {
+
+		HelloWorld hello = new HelloWorld();
+		String result = hello.foo();
+
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals(result, "Hello World!1");
+
+	}
+
+}
